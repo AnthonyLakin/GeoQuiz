1d43a4d50127e2c9c7ff99cf54b68dc5d216f61e
anthonyPC <anthony.s.lakin@outlook.com>
2024-02-18 00:47:25 -0600

    Mostly done

--- app/src/main/java/com/lakin/msu/geoquiz/MainActivity.kt
+++ app/src/main/java/com/lakin/msu/geoquiz/MainActivity.kt
@@ -3,10 +3,12 @@ package com.lakin.msu.geoquiz
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.lakin.msu.geoquiz.databinding.ActivityMainBinding
 
-
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
@@ -20,10 +22,13 @@ class MainActivity : AppCompatActivity() {
     )
 
     private var currentIndex = 0
-
+    private var history = mutableSetOf<Int>(0)
+    private var numerator = 0
+    private var denominator = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        Log.d(TAG, "onCreate (Bundle?) called")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.trueButton.setOnClickListener {
@@ -33,50 +38,114 @@ class MainActivity : AppCompatActivity() {
         binding.falseButton.setOnClickListener {
             checkAnswer(false)
         }
-        //        Exercise 2 snippet below, clicking on text view advances questions
         binding.questionTextview.setOnClickListener {
-            questionHandler("next")
+            questionHandler(true)
         }
         binding.nextButton.setOnClickListener {
-            questionHandler("next")
+            questionHandler(true)
         }
-        //    Exercise 3 snippet below, added previous functionality
         binding.prevButton.setOnClickListener {
-            questionHandler("prev")
+            questionHandler(false)
+        }
+        binding.resetButton.setOnClickListener {
+            resetAll(false)
         }
 
 
     }
 
-    //    Exercise 2 snippet below, clicking on text view advances questions
-    private fun questionHandler(typeState: String) {
-        if (typeState == "next") {
-            currentIndex = (currentIndex + 1) % questionBank.size
-
-            //    Exercise 3 snippet below, added previous functionality
-        } else if (typeState == "prev") {
-            if (currentIndex != 0 ) {
-                currentIndex = (currentIndex - 1) % questionBank.size
+    private fun questionHandler(typeBool: Boolean) {
+        if (typeBool) {
+            if (currentIndex != (questionBank.size - 1)) {
+                currentIndex++
+            } else {
+                currentIndex = (questionBank.size - 1)
             }
-
-
+        } else  {
+            if (currentIndex != 0) {
+                currentIndex--
+            } else {
+                currentIndex = 0
+            }
         }
         updateQuestion()
+        if (currentIndex in history) {
+            binding.trueButton.isEnabled = false
+            binding.falseButton.isEnabled = false
+        } else {
+            binding.trueButton.isEnabled = true
+            binding.falseButton.isEnabled = true
+        }
+
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
-
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
+            val correctAnswer = questionBank[currentIndex].answer
+            val messageResId = if (userAnswer == correctAnswer) {
+                R.string.correct_toast
+            } else {
+                R.string.incorrect_toast
+            }
+            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+            denominator++
+            questionHandler(true)
+            if (messageResId == R.string.correct_toast) {
+                numerator++
+            }
+        if (denominator == questionBank.size) {
+            val result = (numerator.toDouble() / denominator) * 100
+            val formattedResult = String.format("%.1f%%", result)
+            Toast.makeText(this, formattedResult, Toast.LENGTH_SHORT).show()
+            Log.d(TAG, formattedResult)
+            resetAll(true)
         }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+
+        history.add(currentIndex)
     }
 
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         binding.questionTextview.setText(questionTextResId)
     }
+
+    private fun resetAll(currentState: Boolean) {
+        if (currentState) {
+            binding.resetButton.visibility = View.INVISIBLE
+            binding.resetButton.isEnabled = false
+            binding.trueButton.isEnabled = true
+            binding.falseButton.isEnabled = true
+            currentIndex = 0
+            history = mutableSetOf<Int>(0)
+            numerator = 0
+            denominator = 0
+        } else {
+            binding.resetButton.visibility = View.VISIBLE
+            binding.resetButton.isEnabled = true
+        }
+
+
+    }
+
+
+
+/*    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }*/
 }
